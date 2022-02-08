e0d4d12696712ba18fdf38f9f71074483a839a94
webtracer <74070641+webtracer@users.noreply.github.com>
2022-02-08 08:52:40 -0800

    Add files via upload

---
+++ Constants/Constants/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Constants
+{
+    class Program
+    {
+        //variables which are outside any methods, but inside the class
+        //constants as fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+        const string myBirthday = "04/24/1971";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"My birthday is {myBirthday}");
+
+        }
+    }
+}
---
+++ Datatypes_And_Variables/Datatypes_And_Variables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Datatypes_And_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char myChar = 'a';
+            string myString = "This is my string";
+            int myInt = 14;
+            float myFloat = 3.1417f;
+            double myDouble = 20.98756343092384985434;
+            long myLong = 15987654;
+            bool myBool = false;
+
+            string string1 = "I control Text";
+            string string2 = "32";
+
+            Console.WriteLine($"The character I chose was {myChar}");
+            Console.WriteLine($"The string I chose reads '{myString}");
+            Console.WriteLine($"Tne integer I selected is {myInt.ToString()}");
+            Console.WriteLine($"The Floating Point number I put is {myFloat}");
+            Console.WriteLine($"I chose {myDouble} as a Double exanple");
+            Console.WriteLine($"The Long example I picked is {myLong}");
+            Console.WriteLine($"The Boolean value I picked is {myBool.ToString()}");
+            Console.WriteLine();
+            Console.WriteLine($"Instructions require me to use this string: '{string1}'");
+            Console.WriteLine($"The requested integer is {Int32.Parse(string2)}");
+        }
+    }
+}
